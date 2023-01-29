086dd1610c4cc32627f38bb28ee3ba5281451e0a
Luis López Álvarez <luis@MacBook-Pro.local>
2023-01-29 20:41:04 +0100

    findia2

--- 1_OSM_installdescriptors.sh
+++ 1_OSM_installdescriptors.sh
@@ -38,17 +38,3 @@ osm nspkg-create /media/sf_PracticaFinal/rdsv-final/pck/renes_ns.tar.gz
 
 echo "Visualizamos el paquete NS importado"
 osm nsd-list
-
-# Instanciación rennes 1
-#export NSID1=$(osm ns-create --ns_name renes1 --nsd_name renes --vim_account dummy_vim)
-#echo $NSID1
-#export NSID1
-
-# Instanciación rennes 2
-#export NSID2=$(osm ns-create --ns_name renes2 --nsd_name renes --vim_account dummy_vim)
-#echo $NSID2
-#export NSID2
-
-# Visualización de las instancias creadas (esperar hasta que esten READY)
-#echo "Visualizamos las instancias creadas"
-#watch osm ns-list
