7123b6a67a915705db4799c25172d30a4bf4efec
Luis López Álvarez <luis@MacBook-Pro.local>
2023-01-29 17:49:17 +0100

    permisos

--- 0.5_permisos.sh
+++ 0.5_permisos.sh
@@ -1,2 +1,3 @@
 chmod 777 /home/upm/Desktop/rdsv-final/1_OSM_clusterk8s.sh
-chmod 777 /home/upm/Desktop/rdsv-final/2_OSM_instanciacion.sh
+chmod 777 /home/upm/Desktop/rdsv-final/2_OSM_installdescriptors.sh
+chmod 777 /home/upm/Desktop/rdsv-final/3_OSM_instanciacion.sh
