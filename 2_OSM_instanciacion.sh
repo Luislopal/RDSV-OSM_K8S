6e150b4e309ca4f0da0424b51b01f9ed0e8cf578
Luis López Álvarez <luis@MacBook-Pro.local>
2023-01-29 12:13:07 +0100

    rdsv-final_inicio6

--- 2_OSM_instanciacion.sh
+++ 2_OSM_instanciacion.sh
@@ -1,13 +1,24 @@
 # Importamos los paquetes VNF a OSM
 chmod 777 /home/upm/Desktop/rdsv-final/pck
 
-echo "Instalamos lo paquetes necesarios: pycurl, prettytable y packaging"
+#echo "Instalamos lo paquetes necesarios: pycurl, prettytable y packaging"
 
-pip install pycurl
-pip install prettytable
-pip install packaging
+#pip install pycurl
+#pip install prettytable
+#pip install packaging
 
+echo "Creación imagen docker de KNF:access"
+cd /home/upm/Desktop/rdsv-final/img/vnf-img
+sudo docker build -t vnf-img .
 
+echo "Creación imagen docker de KNF:cpe"
+cd /home/upm/Desktop/rdsv-final/img/vnf-rcpe
+sudo docker build -t vnf-vyos .
+
+# Ver las imagenes docker creadas
+docker images
+
+# Importamos los paquetes VNF a OSM
 cd /home/upm/Desktop/rdsv-final/pck
 sudo osm vnfd-create accessknf_vnfd.tar.gz
 sudo osm vnfd-create cpeknf_vnfd.tar.gz
@@ -17,19 +28,20 @@ osm vnfd-list
 
 # Importamos los paquetes NS a OSM
 sudo osm nsd-create renes_ns.tar.gz
+
 echo "Visualizamos el paquete NS importado"
 osm nsd-list
 
 # Instanciación rennes 1
-export NSID1=$(osm ns-create --ns_name renes1 --nsd_name renes --vim_account dummy_vim)
-echo $NSID1
-export NSID1
+#export NSID1=$(osm ns-create --ns_name renes1 --nsd_name renes --vim_account dummy_vim)
+#echo $NSID1
+#export NSID1
 
 # Instanciación rennes 2
-export NSID2=$(osm ns-create --ns_name renes2 --nsd_name renes --vim_account dummy_vim)
-echo $NSID2
-export NSID2
+#export NSID2=$(osm ns-create --ns_name renes2 --nsd_name renes --vim_account dummy_vim)
+#echo $NSID2
+#export NSID2
 
 # Visualización de las instancias creadas (esperar hasta que esten READY)
-echo "Visualizamos las instancias creadas"
-watch osm ns-list
+#echo "Visualizamos las instancias creadas"
+#watch osm ns-list
